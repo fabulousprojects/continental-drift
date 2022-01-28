1753d802d0812d89a439126ae8f94be6e7327897
fabulousprojects <97608310+fabulousprojects@users.noreply.github.com>
2022-01-28 14:59:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function geocentric() {
+    document.getElementById("geocentric").style.color = "blue";
+    document.getElementById("astronomy-contributions").style.color = "black";
+    document.getElementById("other-contributions").style.color = "black";
+    document.getElementById("major-works").style.color = "black";
+    document.getElementById("Quiz").style.color = "black";
+
+    
+    document.getElementById("content").innerHTML = `First person who thought about the continental drift was Abraham Ortelius.
+    He was a dutch Map-Maker who proposed this idea in 1596.
+    Antonio Pelligrini drew a map about three continents together.
+    Then Alfred Wagner gave the continental drift theory in 1912.`
+     
+}
+
+function astronomyContributions() {
+    document.getElementById("geocentric").style.color = "black";
+    document.getElementById("astronomy-contributions").style.color = "blue";
+    document.getElementById("other-contributions").style.color = "black";
+    document.getElementById("major-works").style.color = "black";
+    document.getElementById("Quiz").style.color = "black";
+
+    
+    document.getElementById("content").innerHTML = `According to Wagner there existed a super continent and a mega ocean. The Super continent was called Pangaea and the mega ocean was called Panthalassa. Pangea broke into 2 continental masses. Laurasia and Gondwanaland. `
+}
+
+
+function otherContributions() {
+    document.getElementById("geocentric").style.color = "black";
+    document.getElementById("astronomy-contributions").style.color = "black";
+    document.getElementById("other-contributions").style.color = "blue";
+    document.getElementById("major-works").style.color = "black";
+    document.getElementById("Quiz").style.color = "black";
+
+
+    document.getElementById("content").innerHTML = `1. When Fossils of the same animal appeared in different continents eg.Leamers which existed in india and madagascer (lemuria)
+    <br>
+    2. The shore lines of Africa and South America have an unmistakable match.
+    <br>
+    3. Rocks of same age and type are found accross oceans. I.E : Belt of Ancient Rocks in Brazli's coastline is found in Western Africa.
+    This proves that there didn't exist an ocean separating them two.
+    <br>
+    4.In Brazil and Ghana gold mines are available. Also the first diamonds were found in India followed by South Africa`
+
+}
+
+function majorWorks() {
+    document.getElementById("geocentric").style.color = "black";
+    document.getElementById("astronomy-contributions").style.color = "black";
+    document.getElementById("other-contributions").style.color = "black";
+    document.getElementById("major-works").style.color = "blue";
+    document.getElementById("Quiz").style.color = "black";
+
+    document.getElementById("content").innerHTML = `The causes of continental drift are perfectly explained by the plate tectonic theory. The earth's outer shell is composed of plates that move a little bit every year. Heat coming from the interior of the earth triggers this movement to occur through convection currents inside the mantle.
+    They based their idea of continental drift on several lines of evidence: fit of the continents, paleoclimate indicators, truncated geologic features, and fossils.`
+
+}
+
+function quiz() {
+    document.getElementById("geocentric").style.color = "black";
+    document.getElementById("astronomy-contributions").style.color = "black";
+    document.getElementById("other-contributions").style.color = "black";
+    document.getElementById("major-works").style.color = "black";
+    document.getElementById("Quiz").style.color = "blue";
+    document.getElementById("content").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/uLahVJNnoZ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+}
